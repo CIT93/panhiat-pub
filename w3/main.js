341b53f74862b480e661c45e4e1f8445c4df8f60
panhia-thao <PThao42@my.scccd.edu>
2024-08-29 10:58:30 -0700

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,30 +1,33 @@
-function determineHouseholdPoints() {
-  console.log("inside the function");
-}
-
 let cboFo0t = 0;
-const numInHousehold = 3;
 
-if (numInHousehold === 1) {
-  console.log(cboFo0t + 14);
-} else if (numInHousehold === 2) {
-  console.log(cboFo0t + 12);
-} else if (numInHousehold === 3) {
-  console.log(cboFo0t + 10);
-} else if (numInHousehold === 4) {
-  console.log(cboFo0t + 8);
-} else if (numInHousehold === 5) {
-  console.log(cboFo0t + 6);
-} else if (numInHousehold === 6) {
-  console.log(cboFo0t + 4);
-} else if (numInHousehold > 5) {
-  console.log(cboFo0t + 2);
-} else {
-  console.log(
-    `Based on the number of member of the household of ${numInHousehold} the points would ${cboFo0t}.`
-  );
+function determineHouseholdPoints(numberInHousehold) {
+    if (numberInHousehold === 1) {
+        cboFo0t += 14;
+    } else if (numberInHousehold === 2) {
+        cboFo0t += 12;
+    } else if (numberInHousehold === 3) {
+        cboFo0t += 10;
+    } else if (numberInHousehold === 4) {
+        cboFo0t += 8;
+    } else if (numberInHousehold === 5) {
+        cboFo0t += 6;
+    } else if (numberInHousehold === 6) {
+        cboFo0t += 4;
+    } else if (numberInHousehold > 6) {
+        cboFo0t += 2;
+    } else {
+        console.log("Not a valid number of household members.");
+        return;
+    }
+
+    console.log(`Based on the number of members in the household (${numberInHousehold}), the points would be ${cboFo0t}.`);
 }
 
-//global scope
+// Example usage
+determineHouseholdPoints(3);  
+determineHouseholdPoints(4);  
 
-determineHouseholdPoints();
+
+/*Notes on Fuction can be executed by the function name. It takes in a parameter
+we can give it argument. determineHouseholePoints() this is calling the function. 
+less variable on global scope */
